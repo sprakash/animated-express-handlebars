9789cea268c618cab2d9c52efb91c41bb7868dd5
anusuyarc <anusuyarc89@gmail.com>
2016-04-03 07:21:31 -0700

    moved navigation, scripts, head to partial folders. Wohoooogit add *git add *

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+var hbs = require('hbs');
 
 
 
 app.engine('handlebars', 
 	exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
 
 app.get('/', function(req, res){
 	res.render('index')
@@ -19,6 +22,8 @@ app.get('/about', function(req, res){
 
 
 app.use('/public', express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 
 var port = Number(process.env.PORT || 8080);
