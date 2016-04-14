28447c7d36497d313bc66d9b516bb5c45a8f864f
Anusuya Choudhury <anusuyachoudhury@Anusuyas-MacBook-Pro.local>
2016-04-13 17:16:28 -0700

    fixing about margins

--- public/js/script.js
+++ public/js/script.js
@@ -36,15 +36,18 @@ $(function(){
 	var movementStrength = 50;
 	var height = movementStrength / $(window).height();
 	var width = movementStrength / $(window).width();
-	$(".photo").mousemove(function(e){
-		var pageX = e.pageX - ($(window).width() / 2);
-		var pageY = e.pageY - ($(window).height() / 2);
-		var newvalueX = width * pageX * -1 - 25;
-		var newvalueY = height * pageY * -1 - 25;
-		$(this).css("transform","translate("+ newvalueX+"px, " +newvalueY+"px)");
-		$(this).css("background-size", "cover");
-	});
-
+	
+		$(".photo").mousemove(function(e){
+			if ($(window).width() > 960){
+			var pageX = e.pageX - ($(window).width() / 2);
+			var pageY = e.pageY - ($(window).height() / 2);
+			var newvalueX = width * pageX * -1 - 25;
+			var newvalueY = height * pageY * -1 - 25;
+			$(this).css("transform","translate("+ newvalueX+"px, " +newvalueY+"px)");
+			$(this).css("background-size", "cover");
+		};
+		});
+	
 
 
 	
@@ -81,5 +84,11 @@ $(function(){
 
 	// 	// })
 	// });
+
+
+
 	
 });
+
+
+console.log("kjskljlakjlkjsdlj");
